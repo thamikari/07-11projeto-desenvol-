3426dca21d5d2f6cc6045169f5cfb87b6b60dfdc
thamikari <166084642+thamikari@users.noreply.github.com>
2024-11-14 22:28:32 -0300

    Add files via upload

---
+++ 07-11projeto-desenvol--main/perguntas.js/perguntas.js
@@ -0,0 +1,44 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Adivinha',
+    'o que é,o que é que é de comer e não é para comer?',
+    'a colher'
+)
+criaCartao(
+    'Adivinha',
+    'o que é,o que é que que nasce dechado,cresce fechado,mas tem água dentro?',
+    'o coco'
+)
+criaCartao(
+    'Adivinha',
+    'o que é,o que é  que quanto maior é,menos se ve?',
+    'a escuridão'
+)
+criaCartao(
+    'Adivinha',
+    'o que é,o que é  que está no meio do rio?',
+    'a letra "i"'
+)
